08d09f8ede7b90c00dda0c7a1891457f63013321
JNHNG0205 <heng.teh0205@gmail.com>
2025-12-17 21:32:04 +0800

    done setup

---
+++ backend/src/config/env.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT || 4000,
+  DATABASE_URL: process.env.DATABASE_URL!,
+  REDIS_URL: process.env.REDIS_URL!
+};
